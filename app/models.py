984791848390c41f70b077b4366c58e397974cf3
Walid Zakaria <walidpiano@yahoo.com>
2018-03-15 11:44:38 +0200

    Create early deployment copy

---
+++ app/models.py
@@ -0,0 +1,28 @@
+import datetime
+
+
+class Member:
+
+    def __init__(self, name, age):
+        self.id = 0
+        self.name = name
+        self.age = age
+        self.posts = []
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}"
+
+
+class Post:
+
+    def __init__(self, title, content, member_id = 0):
+        self.id = 0
+        self.title = title
+        self.content = content
+        self.member_id = member_id
+        self.date = datetime.datetime.now()
+
+
+    def __str__(self):
+        return f"Title: {self.title}, content: {self.content}, Time: {self.date}"
+
