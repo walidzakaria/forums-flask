e7b58671786658b403f844820a359835d07db955
Walid Zakaria <walidpiano@yahoo.com>
2018-03-22 00:07:50 +0200

    Try make SPA

---
+++ app/static/main.js
@@ -0,0 +1,117 @@
+
+$.ajax({
+    type: "GET",
+    url: "/api/topic/all",
+    dataType: "json",
+    success: function (data) {
+        $('body').append(arrayToTable(data));
+    }
+});
+
+function arrayToTable(tableData) {
+
+    $(tableData).each(function (i, rowData) {
+        var row1 = $('<tr id="'+rowData.id+'"></tr>');
+        var row2 = $('<tr></tr>');
+        row1.append($('<td><a class="view-topic" id="'+rowData.id+'" href="topic/show/'+rowData.id+'">'+rowData.title+'</a></td>'));
+        row1.append($('<td><a class="update-topic" id="'+rowData.id+'" href="topic/update/'+rowData["id"]+'">edit</a></td>'));
+        row1.append($('<td><a href="javascript:deleteTopic('+rowData.id+')">delete</a></td>'));
+        row2.append($('<div class="show-topic" id="'+rowData.id+'"></div>'));
+        $('table').append(row1);
+        $('table').append(row2);
+    });
+
+}
+
+$(document).ready(function () {
+    $('a').on('click', function (e) {
+        e.preventDefault();
+        var pageRef = $(this).attr('href');
+        var topicID = $(this).attr('id');
+
+        if (this.className == 'add-topic') {
+            callAddEdit(pageRef);
+        }
+        else if (this.className == 'view-topic') {
+            callViewTopic(pageRef, topicID);
+            viewTopic(topicID);
+        } else if (this.className == 'update-topic') {
+            callViewTopic(pageRef, topicID);
+            updateTopic(topicID);
+        };
+
+    });
+
+});
+
+function callAddEdit(pageRefInput) {
+    $.ajax({
+        url: pageRefInput,
+        type: "GET",
+        dataType: "text",
+        success: function (response) {
+
+            $('.other-pages').html(response);
+            $('.other-pages').css('background-color','lightGrey');
+        }
+    });
+}
+
+function callViewTopic(pageRefInput, id) {
+    $.ajax({
+        url: pageRefInput,
+        type: "GET",
+        dataType: "text",
+        success: function (response) {
+
+            $('#'+id+'.show-topic').html(response);
+            $('.show-topic').css('background-color', 'lightGrey');
+        },
+    });
+}
+function viewTopic(id) {
+    $.ajax({
+        url: "/api/topic/show/"+id,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            $('h2.topic-title').text(data.title);
+            $('p.topic-content').text(data.content);
+            $('#'+id+'.show-topic').show(500).css({visibility: "visible"});
+        }
+    });
+};
+
+function updateTopic(id) {
+    $.ajax({
+        url: "/api/topic/show/"+id,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            $("input[name='title']").val(data.title);
+            $("input[name='content']").val(data.content);
+            $('#'+id+'.show-topic').show(500).css({visibility: "visible"});
+        }
+    });
+};
+
+
+function deleteTopic(topicId) {
+    $.ajax({
+        type: "DELETE",
+        url: "/api/topic/delete/"+topicId,
+        dataType: "json",
+        success: function () {
+            alert("Topic deleted successfully!");
+            window.location.reload();
+        }
+    });
+};
+
+function hideTopic() {
+    $(".show-topic").hide(500).css({visibility: "hidden"});
+};
+
+$(".update-form").submit(function () {
+    alert("walid");
+});
--- app/static/topic.update.js
+++ app/static/topic.update.js
@@ -1,26 +1,26 @@
-    $(function () {
-        $("form").submit(function (event) {
-            event.preventDefault();
-            var topicId = $("input[name='title']").attr('id');
-            var topicTitle = $("input[name='title']").val();
-            var topicContent = $("input[name='content']").val();
+$(function () {
+    $("form").submit(function (event) {
+        event.preventDefault();
+        var topicId = $("input[name='title']").attr('id');
+        var topicTitle = $("input[name='title']").val();
+        var topicContent = $("input[name='content']").val();
 
-            var topicData = {
-                "title": topicTitle,
-                "content": topicContent
-            };
+        var topicData = {
+            "title": topicTitle,
+            "content": topicContent
+        };
 
-            $.ajax({
-                type: "PUT",
-                url: "/api/topic/update/"+topicId,
-                data: JSON.stringify(topicData),
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                success: function ()
-                {
-                    alert("Updated topic successfully!");
-                    window.location.href = "/";
-                }
-            });
+        $.ajax({
+            type: "PUT",
+            url: "/api/topic/update/"+topicId,
+            data: JSON.stringify(topicData),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function ()
+            {
+                alert("Updated topic successfully!");
+                window.location.href = "/";
+            }
         });
     });
+});
